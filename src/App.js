598bbd741648a77db2cc84d0e9d2104f743bee67
SANEH69 <sanehthandoh02@gmail.com>
2025-04-26 23:54:30 +0200

    Created a new component and imported it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Footer from './Footer';
 import './App.css';
+import Dictionary from './Dictionary';
 
 function App() {
   return (
     <div className="App">
+      <div className="App-content">
       <header className="App-header">
-       Please type the word you'd like to know the meaning to...
+       Please type the word you'd like to know the meaning of...
       </header>
+      <Dictionary />
       <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+    let [word, setWord] = useState(" ");
+    let [meaning, setMeaning] = useState(" ")
+
+    function showWord(event) {
+        event.preventDefault();
+    }
+
+    function showMeaning(response) {
+        setMeaning(response.data.answer);
+    }
+    
+    let apiKey = "04d1784de2be03a1bd2o2db8tf6b23e4";
+    let context = "You are an English expert that knows meanings to every word.";
+    let prompt = `Please provide a brief meaning of ${word}. Make it a maximum of three lines.`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(showMeaning);
+
+    function searchWord(event) {
+        setWord(event.target.value);
+    }
+
+    let form = (
+        <form onSubmit={showWord}>
+            <input type="search" onChange={searchWord}/>
+            <input type="submit" value="Search" />
+        </form>
+    );
+
+    return(
+        <div>
+        <p>{form}</p>
+        <p className="word">{word}</p>
+        <p>{meaning}</p>
+        </div>
+    );
+}
