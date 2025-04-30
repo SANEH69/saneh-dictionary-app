209e73cbcf9be0ea5bfcee821a50f5ba62cb00e8
SANEH69 <sanehthandoh02@gmail.com>
2025-04-30 14:09:24 +0200

    Cleaned my code in the Meanings.js component

--- src/Meanings.js
+++ src/Meanings.js
@@ -3,23 +3,21 @@ import React from "react";
 export default function Meanings(props) {
     
     if (props.meaning) {
-    return (
-        <div className="Meanings">
-            <h3>{props.meaning.partOfSpeech}</h3>
-            {props.meaning.map(function (meaning, index) {
-                return (
-                    <div key={index}>
-                        <p>{meaning.definitions}</p>
-                        <br />
-                        <p><em>{meaning.example}</em></p>
-                    </div>
-                );
-            })}
-        </div>
-    ) 
-   } else {
         return (
-            <p>Fetching data</p>
+            <div className="Meanings">
+                <h3>{props.meaning.partOfSpeech}</h3>
+                {props.meaning.definitions.map((definition, index) => (
+                    <div key={index}>
+                        <p>{definition.definition}</p>
+                        <br />
+                        <p><em>{definition.example}</em></p>
+                    </div>
+                ))}
+            </div>
         );
-   };
+    } else {
+        return (
+            <p>Fetching data...</p>
+        );
+    }
 }
